ecced89d590b3e45c0408c739e0b123140cfe6bb
Ray <rediatpm@gmail.com>
2026-01-03 22:46:53 -0800

    acme 0.2.5 updates, addition: acme util garmin merge-gencsv, refactor & clean: module_tagblock -> module_categorize, addition: module nickname (module_categorize = cat)

--- usr/app/module_categorize.py
+++ usr/app/module_categorize.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
 
-"""Tagblock Module"""
+"""Categorize Module"""
 
-# Naming refs: https://www.gnu.org/software/make/manual/make.html https://docs.docker.com/reference/dockerfile/
+name      = 'Categorize Module'
+nickname  = 'cat'
 
 """
-Tagblock Module: Categories, Hashtags, & Subtags
-------------------------------------------------
-The tagblock module allows entries to have 2 types of important labels: categories & hashtags.
-  - A tagblock is simply a block of text surrounded by parenthesis that can contain category names, hashtags, and subtags.
-  - A tagblock can only be parsed from the very end of an entry and cannot have any other character after the closing parenthesis.
+Module Options:
+---------------
+- module_categorize : Creates columns for a CSV using the categories inside of parenthesis blocks at the end.
+
+Module Options Usage:
+---------------------
+  Run      Generate CSV    Date Input       Module Options
+  ----------------------------------------------------------------
+  acme     gencsv          {date_input}     module_categorize | cat
+
+"""
+
+
+"""
+Categorize Module: Categories, Hashtags, & $shortcuts
+-----------------------------------------------------
+The categorize module allows entries to have 2 types of important labels: categories & hashtags.
+  - Daily logs can be categorized by adding comma separated lists surrounded by parenthesis at the end of each log.
+  - Categories can only be parsed from the very end of an entry and cannot have any other character after the closing parenthesis.
 Here are 2 valid examples:
  - {rest_of_entry} (category1, category2, category3, category4, category5, category6, category7, category8, category9, category10, #hashtag1 #hash tag 2 #hashtag 3, #hashtag5 #hashtag-number-6){end}
  - {rest_of_entry} (category1 #hashtag one, category2, category number three, #hashtag 2, #hashtag3 #hashtag number four){end}
-A regular parenthesis block can be differentiated from a tagblock by having characters or text after the closing parenthesis. Note the space and semicolons below.
-  - {rest_of_entry} (regular paren block) {end}
-  - {rest_of_entry} (regular paren block);{end}
+A regular parenthesis block can be differentiated from a category block by having characters or text after the closing parenthesis. Note the space and semicolons below.
+  - {rest_of_entry} (regular paren block) {end} <-- ends with space
+  - {rest_of_entry} (regular paren block);{end} <-- ends with semicolon
 
-Tagblock Content Rules
+Categorization Content Rules
 - There are a maximum number of 10 categories allowed. (Subcategories can be conceptualized as successive categories.)
 - There are an umlimited number of hashtags allowed.
-- Categories must be in [a-zA-Z0-9-_\s]+ format.
-- Hashtags must start with '#' and be in [a-zA-Z0-9-_\s]+ format after the '#'.
+- Categories must be in [a-zA-Z0-9-_\\s]+ format.
+- Hashtags must start with '#' and be in [a-zA-Z0-9-_\\s]+ format after the '#'.
 - Commas are not strictly necessary to separate categories and hashtags except where spaces are part of the name of the category or hashtag.
 - Categories and hashtags are parsed in the order of their type. Order mixing between types is allowed as in example 2 above.
 
-Subtags
-- A subtag is simply a shortcut name for a category or hashtag that will be replaced or substituted at parse time.
-- A subtag must start with a '$' for both categories and hashtags.
-- The parser will use the subtag glossary to find and replace the shortcuts in the tagblock with their appropriate substitutions.
+$shortcuts
+- A $shortcut is simply a shortcut name for a category or hashtag that will be replaced or substituted at parse time.
+- A $shortcut must start with a '$' for both categories and hashtags.
+- The parser will use the $shortcut glossary to find and replace the shortcuts in the parenblock with their appropriate substitutions.
 
 Examples:
 - {rest_of_entry} ($zoom)
@@ -38,24 +53,10 @@ Glossary Example:
 - $zoom    :    work, meeting, zoom
 - $fit24   :    #fitness-challenge-2024
 
-In the above examples, the parser will find the subtags ($zoom & $fit24) and replace them with the appropriate categories and hashtags.
-The glossary can contain unlimited definitions for subtags.
+In the above examples, the parser will find the $shortcuts ($zoom & $fit24) and replace them with the appropriate categories and hashtags.
+The glossary can contain unlimited definitions for $shortcuts.
 """
 
-name = 'Tagblock Module'
-
-"""
-Module Options:
----------------
-- module_tagblock.categorize : Creates columns for a CSV using the categories inside of a tagblock.
-
-Module Options Usage:
----------------------
-  Run      Generate CSV    Date Input       Module Options
-  --------------------------------------------------------------------
-  acme     gencsv          {date_input}     module_tagblock.categorize
-
-"""
 
 import re
 
@@ -63,7 +64,7 @@ import re
 
 def apply(entry, glossary):
   """This function is automatically called on each entry if this module is used in 'module_settings.py'!"""
-  entry = replace_subtags(entry, glossary)
+  entry = replace_shortcuts(entry, glossary)
   return entry
 
 
@@ -72,11 +73,11 @@ def apply(entry, glossary):
 
 def options(csv_list, meta={}):
   """Additional final options for the CSV"""
-  """The categorize option adds additional columns for the final CSV using the tagblock categories."""
+  """The categorize option adds additional columns for the final CSV using the parenblock categories."""
 
-  # -- start: module_tagblock.categorize
+  # -- start: module_categorize -- #
 
-  if meta['option'] == 'module_tagblock.categorize':
+  if meta['option'] == 'module_categorize':
 
     csv_header_row_index     = 0
     csv_desc_column_index    = 2
@@ -89,17 +90,17 @@ def options(csv_list, meta={}):
 
     for line in csv_list:
       column_desc = line[csv_desc_column_index].strip('"') # strip double quotes
-      entry_split = split_entry_at_tagblock(column_desc)
+      entry_split = split_entry_at_parenblock(column_desc)
       
       if entry_split:
-        category_lines.append([s.strip() for s in entry_split['tagblock_inside'].split(',')])
-        desc_sans_tagblock = entry_split['rest_of_entry']
+        category_lines.append([s.strip() for s in entry_split['parenblock_inside'].split(',')])
+        desc_sans_parenblock = entry_split['rest_of_entry']
       else:
         category_lines.append([])
-        desc_sans_tagblock = column_desc
+        desc_sans_parenblock = column_desc
 
       new_line = line
-      new_line[csv_desc_column_index] = f'"{desc_sans_tagblock}"' # wrap with double quotes
+      new_line[csv_desc_column_index] = f'"{desc_sans_parenblock}"' # wrap with double quotes
 
       prepared_lines.append(new_line)
 
@@ -140,13 +141,13 @@ def options(csv_list, meta={}):
 
     return result_list
 
-  # -- end: module_tagblock.categorize
+  # -- end: module_parenblock.categorize
 
   return csv_list
 
 
-def split_entry_at_tagblock(entry):
-  """Receives an entry string and separates tagblock & rest of entry."""
+def split_entry_at_parenblock(entry):
+  """Receives an entry string and separates parenblock & rest of entry."""
   pattern = r'^(.*)(\(([a-zA-Z0-9-_,;\s#\$]+)\))$'
   match   = re.search(pattern, entry)
   result  = {}
@@ -154,38 +155,38 @@ def split_entry_at_tagblock(entry):
   if match:
     result = {
       'rest_of_entry'   : match.group(1),
-      'tagblock'        : match.group(2),
-      'tagblock_inside' : match.group(3),
+      'parenblock'        : match.group(2),
+      'parenblock_inside' : match.group(3),
     }
   else:
     result = False
 
   return result
 
-def replace_subtags(entry, glossary):
-  """Receives an entry string and performs substitutions for subtags."""
+def replace_shortcuts(entry, glossary):
+  """Receives an entry string and performs substitutions for $shortcuts."""
   
-  entry_split = split_entry_at_tagblock(entry)
+  entry_split = split_entry_at_parenblock(entry)
   modif_entry = ''
 
   if entry_split:
 
-    rest_of_entry   = entry_split['rest_of_entry']
-    tagblock        = entry_split['tagblock']
-    tagblock_inside = entry_split['tagblock_inside']
+    rest_of_entry     = entry_split['rest_of_entry']
+    parenblock        = entry_split['parenblock']
+    parenblock_inside = entry_split['parenblock_inside']
 
-    subtag_glossary = glossary.subtag_glossary
+    shortcut_glossary = glossary.shortcut_glossary
 
-    for line in subtag_glossary:
+    for line in shortcut_glossary:
       key = line[0]
       val = line[1]
       if isinstance(key, tuple):
         for k in key:
-          tagblock_inside = tagblock_inside.replace(k, val)
+          parenblock_inside = parenblock_inside.replace(k, val)
       else:
-        tagblock_inside = tagblock_inside.replace(key, val)
+        parenblock_inside = parenblock_inside.replace(key, val)
 
-    modif_entry = f'{rest_of_entry}({tagblock_inside})'
+    modif_entry = f'{rest_of_entry}({parenblock_inside})'
 
   else:
     modif_entry = entry
