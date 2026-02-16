cf93f2c64c7926b150289c9f895e8136c8799724
Ray <ray@redment.com>
2026-02-16 14:24:01 -0800

    cmd initial

---
+++ acme/cli/cmd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+
+def curl(url, headers = ''):
+  # -- run curl via subprocess -- #
+  
+  curl_command = f'curl "{url}" -H "{headers}"'
+  process = subprocess.run(curl_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  output = process.stdout.decode('utf-8')
+  error = process.stderr.decode('utf-8')
+  if process.returncode == 0:
+    return output
+  else:
+    return f'Error: {error}'
